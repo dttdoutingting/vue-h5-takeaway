fdcf74cdbeb1cbecadee8e4be25ec9ab401be295
wendy <dttdoutingting@163.com>
2020-01-08 14:07:59 +0800

    设置路由 vue-router
    Signed-off-by: wendy <dttdoutingting@163.com>

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
-  <div id="app" class="app">
-    app template page
+  <div id="app">
+   <router-view />
+   <FooterGuide></FooterGuide>
   </div>
 </template>
 
 <script>
-
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 export default {
   name: 'App',
   components: {
+    FooterGuide
   }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  .app
+  #app
     width 100%
     height 100%
     background #f5f5f5
--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page">
-
+    FooterGuide
   </div>
 </template>
 
