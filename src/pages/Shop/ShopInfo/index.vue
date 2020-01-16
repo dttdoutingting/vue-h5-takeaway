054a7dc4f8c58b45663fa7e060cf2fb721ea1896
wendy <dttdoutingting@163.com>
2020-01-16 16:56:20 +0800

    商家 静态页面
    Signed-off-by: wendy <dttdoutingting@163.com>

--- src/pages/Shop/ShopInfo/index.vue
+++ src/pages/Shop/ShopInfo/index.vue
@@ -1,10 +1,130 @@
 <template>
-  <div class="page">
-    ShopInfo组件
+  <div class="shop-info">
+    <div class="info-content">
+      <section class="section">
+        <h3 class="section-title">配送信息</h3>
+        <div class="delivery">
+          <div>
+            <span class="delivery-icon">硅谷专送</span>
+            <span>由商家配送提供配送，约28分钟送达，距离1000m</span>
+          </div>
+          <div class="delivery-money">配送费￥4</div>
+        </div>
+      </section>
+      <Split />
+      <section class="section">
+        <h3 class="section-title">活动与服务</h3>
+        <div class="activity">
+          <div class="activity-item activity-green">
+            <span class="content-tag">
+              <span class="mini-tag">首单</span>
+            </span>
+            <span class="activity-content">
+              新用户下单立减17元(不与其它活动同享)
+            </span>
+          </div>
+          <div class="activity-item activity-red">
+            <span class="content-tag">
+              <span class="mini-tag">满减</span>
+            </span>
+            <span class="activity-content">
+              满35减19，满65减35
+            </span>
+          </div>
+          <div class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">
+              【立减19.5元】欢乐小食餐</span>
+          </div>
+          <div class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">【立减29元】火烤菠萝皇堡双人餐</span>
+          </div>
+          <div class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">【立减16.5元】火烤菠萝皇堡单人餐</span>
+          </div>
+          <div class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">【立减15】经典安格斯单人餐</span>
+          </div>
+          <div class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">【立减11.95】皇堡双人餐</span>
+          </div>
+          <div class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">【立减19.8】半价单人餐</span>
+          </div>
+        </div>
+      </section>
+      <Split />
+      <section class="section" style="pointer-events: auto;">
+        <h3 class="section-title">商家实景</h3>
+        <div class="pic-wrapper">
+          <ul
+            class="pic-list"
+            style="width: 624px; transition-property: transform;
+            transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);
+            transition-duration: 0ms; transform: translate(0px, 0px) scale(1) translateZ(0px);">
+            <li class="pic-item" style="pointer-events: auto;">
+              <img width="120" height="90" src="https://fuss10.elemecdn.com/f/7f/d1422ec824a0a9d1fb879c57ab533jpeg.jpeg">
+            </li>
+            <li class="pic-item" style="pointer-events: auto;">
+              <img width="120" height="90" src="https://fuss10.elemecdn.com/6/82/2cd3d681f5e93292b3eb49d6b412ajpeg.jpeg">
+            </li>
+            <li class="pic-item" style="pointer-events: auto;">
+              <img width="120" height="90" src="https://fuss10.elemecdn.com/8/93/4cf527b6462eea634f69755374f88jpeg.jpeg">
+            </li>
+            <li class="pic-item" style="pointer-events: auto;">
+              <img width="120" height="90" src="https://fuss10.elemecdn.com/3/f2/2a8796ba025a5773fd685a95ac369jpeg.jpeg">
+            </li>
+            <li class="pic-item" style="pointer-events: auto;">
+              <img width="120" height="90" src="https://fuss10.elemecdn.com/a/c4/5a78f477fd616a51ce33586a76ddbjpeg.jpeg">
+            </li>
+          </ul>
+        </div>
+      </section>
+      <Split />
+      <section class="section" style="pointer-events: auto;">
+        <h3 class="section-title">商家信息</h3>
+        <ul class="detail">
+          <li>
+            <span class="bold">品类</span>
+            <span>包子粥店, 简餐</span>
+          </li>
+          <li>
+            <span class="bold">商家电话</span>
+            <span>18501083744</span>
+          </li>
+          <li>
+            <span class="bold">地址</span>
+            <span>北京市丰台区太平桥44号</span>
+          </li>
+          <li>
+            <span class="bold">营业时间</span>
+            <span>09:35-24:00</span>
+          </li>
+        </ul>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
   name: '',
   components: {},
@@ -12,8 +132,124 @@ export default {
     return {
 
     }
+  },
+  mounted() {
+    new BScroll('.shop-info')
+    new BScroll('.pic-wrapper', {
+      scrollX: true // 水平滑动
+    })
   }
 }
 </script>
 
-<style scoped lang="stylus"></style>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../../common/stylus/mixins.styl"
+
+  .shop-info
+    position absolute
+    top 195px
+    bottom 0
+    left 0
+    width 100%
+    background #fff
+    overflow hidden
+    .section
+      padding 16px 14px 14px
+      font-size 16px
+      background-color #fff
+      color #666
+      border-bottom 1px solid #eee
+      position relative
+      .section-title
+        color #000
+        font-weight 700
+        line-height 16px
+        > .iconfont
+          float right
+          color #ccc
+      .delivery
+        margin-top 16px
+        font-size 13px
+        line-height 18px
+        .delivery-icon
+          width 55px
+          font-size 11px
+          margin-right 10px
+          display inline-block
+          text-align center
+          color #fff
+          background-color #0097ff
+          padding 1px 0
+          border-radius 4px
+        .delivery-money
+          margin-top 5px
+
+      .activity
+        margin-top 16px
+        .activity-item
+          margin-bottom 12px
+          display flex
+          font-size 13px
+          align-items center
+          &.activity-green
+            .content-tag
+              background-color rgb(112, 188, 70)
+          &.activity-red
+            .content-tag
+              background-color rgb(240, 115, 115)
+          &.activity-orange
+            .content-tag
+              background-color: rgb(241, 136, 79)
+          .content-tag
+            display inline-block
+            border-radius 2px
+            width 36px
+            height: 18px
+            margin-right 10px
+            color #fff
+            font-style normal
+            position relative
+            .mini-tag
+              position absolute
+              left 0
+              top 0
+              right -100%
+              bottom -100%
+              font-size 24px
+              transform scale(.5)
+              transform-origin 0 0
+              display flex
+              align-items center
+              justify-content center
+      .pic-wrapper
+        width: 100%
+        overflow: hidden
+        white-space: nowrap
+        margin-top 16px
+        .pic-list
+          font-size: 0
+          .pic-item
+            display: inline-block
+            margin-right: 6px
+            width: 120px
+            height: 90px
+            &:last-child
+              margin: 0
+
+      .detail
+        margin-bottom -16px
+        > li
+          display flex
+          justify-content space-between
+          align-items center
+          margin-right -10px
+          padding 16px 12px 16px 0
+          line-height 16px
+          bottom-border-1px(#ddd)
+          font-size 13px
+          > .bold
+            font-weight 700
+            color #333
+          &:last-child
+            border-none()
+</style>
